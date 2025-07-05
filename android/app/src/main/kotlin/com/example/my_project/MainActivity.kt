5b3580b19b3820f014be9c7e664c7fb9551d6580
TAHA OĞUL <139348240+tahaogul@users.noreply.github.com>
2025-07-05 11:34:49 +0300

    trhghfd

---
+++ android/app/src/main/kotlin/com/example/my_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.app.girasordernek
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/auth2/auth2_model.dart
@@ -0,0 +1,32 @@
+import '/flutter_flow/flutter_flow_animations.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'dart:math';
+import 'dart:ui';
+import '/index.dart';
+import 'auth2_widget.dart' show Auth2Widget;
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+class Auth2Model extends FlutterFlowModel<Auth2Widget> {
+  ///  State fields for stateful widgets in this page.
+
+  // State field(s) for TabBar widget.
+  TabController? tabBarController;
+  int get tabBarCurrentIndex =>
+      tabBarController != null ? tabBarController!.index : 0;
+  int get tabBarPreviousIndex =>
+      tabBarController != null ? tabBarController!.previousIndex : 0;
+
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {
+    tabBarController?.dispose();
+  }
+}
