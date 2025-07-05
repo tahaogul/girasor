820c0c3c7b23c3976b8a64242fe29ce66fde5a94
TAHA OĞUL <139348240+tahaogul@users.noreply.github.com>
2025-07-05 07:23:18 +0300

    ghnvb

---
+++ girasor_dernek/lib/girasor/girasor_model.dart
@@ -0,0 +1,18 @@
+import '/flutter_flow/flutter_flow_icon_button.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_web_view.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import '/index.dart';
+import 'girasor_widget.dart' show GirasorWidget;
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+class GirasorModel extends FlutterFlowModel<GirasorWidget> {
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {}
+}
---
+++ girasor_dernek/lib/index.dart
@@ -0,0 +1,3 @@
+// Export pages
+export '/girasor/girasor_widget.dart' show GirasorWidget;
+export '/auth2/auth2_widget.dart' show Auth2Widget;
