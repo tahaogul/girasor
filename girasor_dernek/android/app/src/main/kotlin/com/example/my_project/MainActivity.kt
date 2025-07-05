5c666ad0a7625e3758b311ada43858d3e7e643cc
TAHA OĞUL <139348240+tahaogul@users.noreply.github.com>
2025-07-05 07:25:46 +0300

    yufjmhvnb

--- girasor_dernek/android/app/src/main/kotlin/com/example/my_project/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.app.girasordernek
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- girasor_dernek/lib/index.dart
+++
@@ -1,3 +0,0 @@
-// Export pages
-export '/girasor/girasor_widget.dart' show GirasorWidget;
-export '/auth2/auth2_widget.dart' show Auth2Widget;
